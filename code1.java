081b67538b4ecac9cd8901d78c4b4daffac3de69
Tanawat6509650120 <tanawat.pua@dome.tu.ac.th>
2023-11-07 22:07:15 +0700

    modified:   code1.java

--- code1.java
+++ code1.java
@@ -6,4 +6,6 @@ public void Hello {
 	System.out.println("6509650476");
 	System.out.println("My name is duanghathai");
 	System.out.println("6509650401 ");
+	System.out.println("My name is Tanawat");
+	System.out.println("6509650120 ");
 }
