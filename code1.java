103133edc2f76fb2ce4986dd291f018e956bb80d
Teerawat6509650476 <teerwat.nak@dome.tu.ac.th>
2023-11-07 13:16:51 +0700

    modified:   code1.java

--- code1.java
+++ code1.java
@@ -2,4 +2,6 @@ public void Hello {
 	System.out.println("Hello");
 	System.out.println("My name is Kanchanop");
 	System.out.println("6509650229");
+	System.out.println("My name is Teerawat");
+	System.out.println("6509650476");
 }
