9ee309f18bee265a4167009486787f9a48ac8e22
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2023-11-13 17:54:15 +0700

    update quit form repo

--- Repo ver 1.0/repository/FormRepository.java
+++ Repo ver 1.0/repository/FormRepository.java
@@ -1,7 +1,9 @@
 package th.ac.tu.cs.Website.repository;
 
 import th.ac.tu.cs.Website.model.AddDrop;
+import th.ac.tu.cs.Website.model.Quit;
 
 public interface FormRepository {
     void saveAddDrop(AddDrop addDropSubject);
+    void saveQuitForm(Quit quit);
 }
